0a3d0b9421e87ff118c577d6700fb8220e97408a
Guilherme068 <81178917+Guilherme068@users.noreply.github.com>
2021-04-26 18:04:01 -0300

    Create atividade12.py

---
+++ atividade12.py
@@ -0,0 +1,101 @@
+#inicio
+def maior_numero(num1, num2, num3)
+ if(num1 > num2 and num1 > num3):
+   print (num1)
+ elif (num2 > num1 and num2> num3):
+   print (num2)
+ elif (num3 > num1 and num3> num2):
+   print (num3)
+#fim 
+
+#inicio
+def converter_string(str)
+  return str.split(0
+#fim
+
+#inicio
+def soma_lista(list)
+  i = 0
+  sum= 0
+  while(i < len(list) ):
+    sum += list[1]
+  return sum
+#fim
+
+#inicio
+def criar_string(list):
+  retourn " ".join(list) 
+# fim
+
+                   #inicio Lista de numeros
+lista = [3,8,9,1,0,2]
+print (lista)
+# fim 
+
+#inicio
+lista_numeros = [3,8,9,1,0,2]
+print (sum(lista))
+#fim
+
+#inicio
+lista = [3,8,9,1,0,2]
+print (sum(lista) / len(lista) )
+# fim
+
+#inicio
+lista = [3,8,9,1,0,2]
+print ( lista.join("|", lista) ) 
+# fim
+
+                   #inicio
+lista.sort = [3,8,9,1,0,2]
+print (lista)
+#fim
+
+#inicio
+
+lista = [3,8,9,1,0,2]
+print (lista randon(lista))
+# fim
+
+ #inicio
+lista = [ 4, 5, 2, 0, 9 ]
+print(clone_lista)
+#fim 
+
+                   
+                   #inicio
+lista = [ 4, 5, 2, 0, 9 ]
+print (lista[:3])
+# fim
+
+#inicio
+lista = [ 4, 5, 2, 0, 9 ]
+print (lista[:-2])
+#Fim
+
+                   
+                  #inicio
+print(list(range(10)) ) 
+#fim
+
+
+#inicio
+print([2*x for x in range(10)] )
+#fim
+
+#inicio
+3print(randint(0,20))
+#fim
+
+                   
+                   #inicio
+lista = ["alo", "Alo", "aLO", "alO"] 
+print (join (upper(lista)))
+#fim
+
+
+#inicio 
+lista = ["alo", "Alo", "aLO", "alO"] 
+print (join (" ", lower(lista)))
+#fim
